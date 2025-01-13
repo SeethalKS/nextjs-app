eb499b5dd2751f36cfe837f4fec6a7fdcf4598ac
SeethalKS <seethal007@outlook.com>
2025-01-13 14:11:38 +0530

    home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
     <div>
   <h1>This is home</h1>
   <Image src={'/Image/shop.jpg'} alt="sample image" width={600} height={400}/>
+  <Image src={'/Image/shop.jpg'} alt="sample image" width={600} height={400}/>
+  <Image src={'/Image/shop.jpg'} alt="sample image" width={600} height={400}/>
   </div>
   );
 }
